cfa301220ca76ec7342cc474967bcbb4d54efc18
mrProger <60040129+mrProger@users.noreply.github.com>
2021-10-08 15:20:58 +0600

    Upload all files

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function GitHubLinkOpen() 
+{
+    window.open('https://github.com/mrProger');
+}
+
+function VkLinkOpen() 
+{
+    window.open('https://vk.com/ikhrebtov');
+}
+
+function TelegramLinkOpen() 
+{
+    window.open('https://t.me/Yatov');
+}
